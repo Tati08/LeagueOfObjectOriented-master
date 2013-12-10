ed8a2fbadcbcfc27ae5cdcee7a0fb4b3ae1032b7
Tati08 <tatiana_diorio@hotmail.com>
2013-12-10 16:59:46 -0200

    add uma frase

---
+++ LeagueOfObjectOriented/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  LeagueOfObjectOriented
+//
+//  Created by Vinicius Miana on 12/8/13.
+//  Copyright (c) 2013 Vinicius Miana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Oi");
+        
+        
+        
+        
+        
+    }
+    return 0;
+}
+
